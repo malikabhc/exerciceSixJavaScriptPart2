00c5ee686e545d542b96d6d5220e786f5cf13865
Malikabhc <malika.bouhcine@gmail.com>
2018-06-25 14:17:12 +0200

    correction

--- assets/js/script6.js
+++ assets/js/script6.js
@@ -1,14 +1,6 @@
-function ChangesImagesOver() {
-  document.getElementById('image1').src = 'assets/img/image1_2.jpg';
-  document.getElementById('image2').src = 'assets/img/image2_2.jpg';
-  document.getElementById('image3').src = 'assets/img/image3_2.jpg';
-  document.getElementById('image4').src = 'assets/img/image4_2.jpg';
-  document.getElementById('image5').src = 'assets/img/image5_2.jpg';
+function ChangesImagesOver(id) {
+  document.getElementById(id).src = 'assets/img/' + id + '_2.jpg';
 }
-function ChangesImagesOut() {
-  document.getElementById('image1').src = 'assets/img/image1.jpg';
-  document.getElementById('image2').src = 'assets/img/image2.jpg';
-  document.getElementById('image3').src = 'assets/img/image3.jpg';
-  document.getElementById('image4').src = 'assets/img/image4.jpg';
-  document.getElementById('image5').src = 'assets/img/image5.jpg';
+function ChangesImagesOut(id) {
+  document.getElementById(id).src = 'assets/img/' + id + '.jpg';
 }
